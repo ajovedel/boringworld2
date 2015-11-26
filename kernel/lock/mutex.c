e684cbda0f52d967aa5eb82a693f1c7d9ba9455c
alex <ajovedel@andrew.cmu.edu>
2015-11-26 16:59:08 -0500

    finished mutexes... except for priorities part

--- kernel/lock/mutex.c
+++ kernel/lock/mutex.c
@@ -122,14 +122,71 @@ int mutex_lock(int mutex  __attribute__((unused)))
 	cur_mutex->bLock = 1;
 	cur_mutex->pHolding_Tcb = cur_tcb;
 	cur_tcb->holds_lock = 1;	//do i need to set holds_lock? what for?
+
+	// part 2 stuff - PRIORITY INVERSION!!
+	// 	1. find highest priority task in sleep queue
+	// 	2. set cur_tcb to use the priority of the highest priority in sleep queue
+	// 	3. when cur_tcb unlocks the mutex, we set the priority back
 	
+
 	enable_interrupts();
 
 	return 0;
 }
 
+/*
+ * @brief Unlock the indicated mutex
+ */
 int mutex_unlock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	disable_interrupts();
+
+	// check mutex id is valid
+	if(!(mutex >= 0 && mutex <= OS_NUM_MUTEX)){
+		enable_interrupts();
+		return -EINVAL;
+	}
+
+	mutex_t *cur_mutex = &(gtMutex[mutex]);
+
+	// check mutex has been created
+	if(cur_mutex->bAvailable == 0){
+		enable_interrupts();
+		return -EINVAL;
+	}
+
+	tcb_t *cur_tcb = get_cur_tcb();
+
+	// check current task is holding the lock
+	if(cur_tcb != cur_mutex->pHolding_Tcb){
+		enable_interrupts();
+		return -EPERM;
+	}
+
+	// dissassociate current task from the mutex
+	cur_tcb->holds_lock = 0;
+	cur_tcb->sleep_queue = NULL;
+
+	// check if other tasks are waiting to use this mutex
+	// if so, change mutex ownership, update sleep queue and add task to run queue
+	if(cur_mutex->pSleep_queue != NULL){
+		cur_mutex->pHolding_Tcb = cur_mutex->pSleep_queue;
+		cur_mutex->pSleep_queue = cur_tcb->sleep_queue;
+		runqueue_add(cur_mutex->pHolding_Tcb, cur_mutex->pHolding_Tcb->cur_prio);
+	}	
+
+	// if no other tasks wants this mutex, free the mutex
+	else{
+		cur_mutex->pHolding_Tcb = NULL;
+		cur_mutex->bLock = 0;
+	}
+	
+	// part 2 stuff - PRIORIY INVERSION!!
+	// need to set back the native priority of the task
+	// (in case it was using priority inversion)
+
+	enable_interrupts();
+
+	return 0;
 }
 
